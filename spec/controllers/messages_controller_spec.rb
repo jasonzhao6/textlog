41e1ee0d029bbb6b6645ba7bf9125cfeffbf00ee
jasonzhao6 <jasonzhao6@gmail.com>
2013-05-23 22:55:07 -0700

    Add test for receiving text message from Twillio.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe MessagesController do
-
+  describe ".create" do
+    context "when Twillio forwards a message" do
+      let(:body) { 'Hello' }
+      let(:params) { { 'AccountSid' => 'sid',
+                       'Body' => body,
+                       'ToZip' => 'zip',
+                       'FromState' => 'state',
+                       'ToCity' => 'city',
+                       'SmsSid' => 'sid',
+                       'ToState' => 'state',
+                       'To' => '+number',
+                       'ToCountry' => 'country',
+                       'FromCountry' => 'country',
+                       'SmsMessageSid' => 'sid',
+                       'ApiVersion' => 'version',
+                       'FromCity' => 'city',
+                       'SmsStatus' => 'status',
+                       'From' => '+number',
+                       'FromZip' => 'zip' } }
+                     
+      before(:each) do
+        post :create, params
+      end
+      
+      specify { Message.count.should == 1 }
+      specify { Message.first.body.should == body }
+    end
+  end
 end
