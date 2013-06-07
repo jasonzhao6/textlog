34ad80d6ab2929af200a628c0061b66f8e843ba9
jasonzhao6 <jasonzhao6@gmail.com>
2013-06-06 22:55:09 -0700

    Wire up parse page to post to activities#create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,12 @@
 class ActivitiesController < ApplicationController
+  def create
+    message = Message.find(params[:message_id])
+    rules_engine = RulesEngine.new(message)
+    rules_engine.execute
+    rules_engine.save
+    redirect_to :activities
+  end
+  
   def index
     
   end
