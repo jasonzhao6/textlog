fc2f7074eb31004c2f14adaefe8de2416b33ee98
jasonzhao6 <jasonzhao6@gmail.com>
2013-06-13 22:39:59 -0700

    Make rule partial more mobile friendly
    Replace matcher/setter command labes with icons.
    
    Colorcode matchers blue and setters green, same colors as their labels.

--- lib/lookup_tables.rb
+++ lib/lookup_tables.rb
@@ -6,4 +6,13 @@ SETTER_PLACEHOLDERS = {
   set_reps: "{ reps: '12' }",
   set_note: "{ note: 'Felt engaged' }",
   nil => "string, hash, or leave blank for regex match data",
-}
+}.with_indifferent_access
+
+SETTER_ICONS = {
+  set_activity: 'flag',
+  add_friend: 'user',
+  add_duration: 'time',
+  set_distance: 'road',
+  set_reps: 'repeat',
+  set_note: 'comment',
+}.with_indifferent_access
