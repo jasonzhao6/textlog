131c1c719ab24ed1bc7c723c5296fc3a5c3bb41a
jasonzhao6 <jasonzhao6@gmail.com>
2013-07-16 00:35:54 -0700

    Rule must have at least 1 setter

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,37 +1,38 @@
 class RulesController < ApplicationController
   before_filter :custom_redirect_path, only: [:bump, :create, :destroy, :edit, :new, :update]
   before_filter :must_be_logged_in, only: [:bump, :create, :destroy, :update]
-  
+
   def bump
     Rule.find(params[:id]).touch
     redirect_to @redirect_path, notice: 'Rule was successfully bumped up.'
   end
-  
+
   def create
     @rule = matcher = Rule.new(matcher_params)
-    if matcher.save
-      create_setters
+    if matcher.valid? && setters_valid?
+      matcher.save
+      setters_create
       redirect_to @redirect_path, notice: 'Rule was successfully created.'
     else
       render action: :new
     end
   end
-  
+
   def destroy
     Rule.find(params[:id]).destroy
     redirect_to @redirect_path, alert: 'Rule was successfully deleted.'
   end
-  
+
   def edit
     @rule = Rule.matchers.find(params[:id])
   end
-  
+
   def index
     # Sidebar
     @command_counts = Activity::COMMANDS.map do |command|
                         Rule.matchers_for(command).count
                       end
-    
+
     # Main
     @rules = if params[:command].present?
                Rule.matchers_for(params[:command])
@@ -47,46 +48,56 @@ class RulesController < ApplicationController
     @redirect_path = rules_path(command: params[:command],
                                 'sort-by' => params['sort-by'])
   end
-  
+
   def new
     @rule = Rule.new
     @message = Message.find(params[:message_id]) if params[:message_id]
   end
-  
+
   def update
     @rule = matcher = Rule.find(params[:id])
-    if matcher.update_attributes(matcher_params)
+    matcher.arg = params[:rule][:arg]
+    if matcher.valid? && setters_valid?
+      matcher.save
       matcher.setters.destroy_all
-      create_setters
+      setters_create
       redirect_to @redirect_path, notice: 'Rule was successfully updated.'
     else
       render action: :edit
     end
   end
-  
+
   private
-    
-    # 
+
+    #
     # Before filters
-    # 
+    #
     def custom_redirect_path
       @redirect_path = params[:redirect_path] || :rules
     end
-    
-    # 
+
+    #
     # Helpers
-    # 
-    def create_setters
+    #
+    def setters_valid?
+      if params[:commands].nil? || params[:commands].join.blank?
+        @rule.errors.add(:rule, 'must have at least 1 setter') and return false
+      else
+        true
+      end
+    end
+
+    def setters_create
       if params[:commands].present?
         params[:commands].zip(params[:args]).map do |command, arg|
           Rule.create(command: command, arg: arg, matcher_id: @rule.id)
         end
       end
     end
-    
-    # 
+
+    #
     # Strong parameters
-    # 
+    #
     def matcher_params
       params.require(:rule).permit(:command, :arg)
     end
